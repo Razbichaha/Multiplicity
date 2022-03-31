a4928619381c81ac3f46f424b48259a5a02b2e52
Сергей <seregabur@live.ru>
2022-03-31 23:19:09 +0300

    2

--- Program.cs
+++ Program.cs
@@ -6,28 +6,23 @@ namespace Кратность
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
+            Random NumberRandom = new Random();
 
-            int n = rnd.Next(1,27);
-            Console.WriteLine("Число - " + n);
+            int randomNumber = NumberRandom.Next(1,27);
+            Console.WriteLine("Число - " + randomNumber);
 
             for(int i =100;i<1000;i++)
             {
-                //для проверки
-                if (i % n == 0) { Console.Write(i+" - "); }
-                //
-                bool flag = true;
-                int q=0 ;
-                while(flag)
+                bool stopCicle = true;
+                int summand = 0;
+
+                while(stopCicle)
                 {
-                    q = q + n;
-                    if (q == i) { Console.WriteLine(i); flag = false; }
-                    if (q > i){ flag = false; }
+                    summand = summand + randomNumber;
+                    if (summand == i) { Console.WriteLine(i); stopCicle = false; }
+                    if (summand > i){ stopCicle = false; }
                 }
-
-
             }
-
         }
     }
 }
